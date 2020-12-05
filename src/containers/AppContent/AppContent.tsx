a829106fe3980a06e64115e458241cb76b708c7e
QuocDat1994 <quocdat1994@gmail.com>
2020-12-05 14:38:20 +0700

    Add react-router

--- src/containers/AppContent/AppContent.tsx
+++ src/containers/AppContent/AppContent.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import {Switch,Route} from "react-router-dom";
 import { Layout } from 'antd';
+
+import { Home } from '../Home/Home';
+import { Cart } from '../Cart/Cart';
+import { Category } from '../Category/Category';
+import { Product } from '../Product/Product';
+
+import "./AppContent.less";
 
 export const AppContent = () => {
   const { Content} = Layout;
@@ -7,6 +15,20 @@ export const AppContent = () => {
   return (
     <Content>
       My Content
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/category">
+          <Category />
+        </Route>
+        <Route path="/product">
+          <Product />
+        </Route>
+      </Switch>
     </Content>
   )
 }
---
+++ src/containers/Category/Category.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./Category.less";
+
+export const Category = () => {
+  return (
+    <div>
+      Category
+    </div>
+  )
+}
