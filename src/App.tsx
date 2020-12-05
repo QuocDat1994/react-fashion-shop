4b983ca2cf5d7954a66166cddef26e452ece53d8
Dat <dat.truong@metanet.co.kr>
2020-12-05 14:22:30 +0700

    Add Less, antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import "./App.css";
+
+import "./App.less";
+import { AppContent } from "./containers/AppContent/AppContent";
+import { AppFooter } from "./containers/AppFooter/AppFooter";
+import { AppHeader } from "./containers/AppHeader/AppHeader";
 
 function App() {
   return (
     <div className="App">
-      dsadas
+      <AppHeader/>
+      <AppContent/>
+      <AppFooter/>
     </div>
   );
 }
---
+++ src/containers/AppContent/AppContent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout } from 'antd';
+
+export const AppContent = () => {
+  const { Content} = Layout;
+
+  return (
+    <Content>
+      My Content
+    </Content>
+  )
+}
