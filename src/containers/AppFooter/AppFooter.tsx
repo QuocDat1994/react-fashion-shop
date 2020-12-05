5620d4bc5bfe08142944483d42f8444ef681be3d
QuocDat1994 <quocdat1994@gmail.com>
2020-12-05 14:22:30 +0700

    Add Less, antd

---
+++ src/containers/AppFooter/AppFooter.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Layout } from 'antd';
+
+import "./AppFooter.less";
+
+export const AppFooter = () => {
+  const { Footer } = Layout;
+
+  return (
+    <div>
+      dsadsa
+    </div>
+  )
+}
---
+++ src/containers/AppHeader/AppHeader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+import "./AppHeader.less";
+
+export const AppHeader = () => {
+  const { Header} = Layout;
+
+  return (
+    <Header className="test">
+      asdsad
+    </Header>
+  )
+}
